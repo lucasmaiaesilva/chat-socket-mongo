61cdf2413952af891213cac9ab0498c77376562d
Lucas Maia e Silva <lucasmaiaesilva@gmail.com>
2016-10-04 11:35:21 -0300

    add mongo-watch package

--- app.js
+++ app.js
@@ -8,9 +8,41 @@ var MsgSocket = require('./models/modelMsg');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/socketio', { poolSize: 1 });
 
+
+var listener = require('mongo-watcher');
+
+var options = {
+  database: 'socketio'
+};
+
+var changeStream = listener.listen(options);
+
+changeStream.on('data', function(data) {
+  pegarInformacoes();
+});
+
+function pegarInformacoes(){
+  MsgSocket.find(function(err, msgs){
+    if(err)
+      return err;
+
+    io.emit('news', msgs);
+  });
+}
+
 // config to receive posts
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+io.on('connection', function(socket){
+  console.log('um usuário conectou');
+
+  pegarInformacoes();
+
+  socket.on('disconnect', function(){
+     console.log('usuario desconectou');
+   });
+});
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/front/index.html');
@@ -24,24 +56,11 @@ app.post('/msg', function(req, res){
   msg.save(function(err){
     if(err)
       res.json(err);
-    res.json({message: 'postagem feita com sucesso'});
+    res.json({ message: 'postagem feita com sucesso' });
   });
 });
 
-io.on('connection', function(socket){
-  console.log('um usuário conectou');
 
-  MsgSocket.find(function(err, msgs){
-    if(err)
-      return err;
-    socket.emit('news', msgs);
-  });
-
-
-  socket.on('disconnect', function(){
-     console.log('usuario desconectou');
-   });
-});
 
 http.listen(8080, function(){
   console.log('listening on *:8080');
